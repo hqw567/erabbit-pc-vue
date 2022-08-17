c0b37a7279c26e54be7b5dbc84528cccd9297730
Herway <hqw6@qq.com>
2022-08-17 10:18:49 +0800

    first: commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <!-- <h1>{{ $store.state.userName }}</h1>
+    <h1>{{ $store.getters['getName'] }}</h1>
+    <button @click="updateName">修改</button>
+    <button @click="delayUpdateName">延迟修改</button> -->
+    <p>{{ $store.state.moduleA.userName }}</p>
+    <p>{{ $store.getters.newName }}</p>
+    <button @click="updateA">点击修改A</button>
+    <button @click="aNewName">点击延迟修改A</button>
+    <hr />
+    <p>{{ $store.state.moduleB.userName }}</p>
+    <p>{{ $store.getters['moduleB/newName'] }}</p>
+    <button @click="updateB">点击修改B</button>
+    <button @click="bNewName">点击延迟修改A</button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup() {
+    const store = useStore()
+    const updateA = () => {
+      store.commit('updateName', '修改A')
     }
+    const aNewName = () => {
+      store.dispatch('aNewName', '我延迟修改了！')
+    }
+    const updateB = () => {
+      store.commit('moduleB/updateName', 'B')
+    }
+    const bNewName = () => {
+      store.dispatch('moduleB/bNewName', '我延迟修改了b！')
+    }
+    return { updateA, aNewName, updateB, bNewName }
+    // const updateName = () => {
+    //   store.commit('updateName', '我修改了代码！嘿嘿')
+    // }
+    // const delayUpdateName = () => {
+    //   store.dispatch('delayUpdateName', '就是要修改你咋滴！')
+    // }
+    // console.log(store.state.userName)
+    // console.log(store.getters.getName)
+    // return { updateName, delayUpdateName }
   }
 }
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,77 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+const moduleA = {
   state: {
+    userName: 'moduleA'
   },
   getters: {
+    newName(state) {
+      return state.userName + '!'
+    }
   },
   mutations: {
+    updateName(state, v) {
+      state.userName = v
+    }
   },
   actions: {
+    aNewName({ commit }, v) {
+      setTimeout(() => {
+        commit('updateName', v)
+      }, 1000)
+    }
+  }
+}
+
+const moduleB = {
+  namespaced: true,
+  state: {
+    userName: 'moduleB'
   },
+  getters: {
+    newName(state) {
+      return state.userName + '!'
+    }
+  },
+  mutations: {
+    updateName(state, v) {
+      state.userName = v
+    }
+  },
+  actions: {
+    bNewName({ commit }, v) {
+      setTimeout(() => {
+        commit('updateName', v)
+      }, 1000)
+    }
+  }
+}
+
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
+
+// export default createStore({
+//   state: {
+//     userName: 'he'
+//   },
+//   getters: {
+//     getName(state) {
+//       return state.userName + '!!'
+//     }
+//   },
+//   mutations: {
+//     updateName(state, name) {
+//       state.userName = name
+//     }
+//   },
+//   actions: {
+//     delayUpdateName({ commit }, v) {
+//       setTimeout(() => commit('updateName', v), 1000)
+//     }
+//   },
+//   modules: {}
+// })
